688049fcab57e90b575cfbf8b295164b8c380bfe
PlatfozaAndreyBorisov <andrey.borisov@platfoza.com>
2021-10-31 18:00:01 +0300

    hicaryCP added

--- src/main/java/com/java/test/config/JdbcConfig.java
+++ src/main/java/com/java/test/config/JdbcConfig.java
@@ -1,23 +1,27 @@
 package com.java.test.config;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
+
 
 @Configuration
 @NoArgsConstructor
 @PropertySource("classpath:application.properties")
 public class JdbcConfig {
 
+    private static final Integer MAX_POOL_SIZE = 10;
+
+
     @Value("${database.url}")
-    private String URl;
+    private String DB_URl;
 
     @Value("${database.username}")
     private String USERNAME;
@@ -27,12 +31,15 @@ public class JdbcConfig {
 
     @Bean
     public DataSource postgresqlDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl(URl);
-        dataSource.setUsername(USERNAME);
-        dataSource.setPassword(PASSWORD);
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setDriverClassName("org.postgresql.Driver");
+        hikariConfig.setJdbcUrl(DB_URl);
+        hikariConfig.setUsername(USERNAME);
+        hikariConfig.setPassword(PASSWORD);
+        hikariConfig.setMaximumPoolSize(MAX_POOL_SIZE);
+        hikariConfig.setPoolName("springHikariCP");
 
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
         return dataSource;
     }
 
